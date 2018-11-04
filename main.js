fd9ceac435fcc0995842513e48c2c42a156be319
Ryan Johnson <ryj1023@yahoo.com>
2018-11-04 17:11:57 -0500

    update

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ $('.right-column').css('height', $('#sticky-content-container').height())
 	
 	if ((scrollTop > stickyStartOffset) && scrollTop + $('#sticky-div').height() < stickyContentContainerHeight) {
 	 	$('#sticky-div').css({ position: 'fixed', top: '0', bottom: 'unset'})
-
     } else if (scrollTop + $('#sticky-div').outerHeight() > stickyContentContainerHeight) {
     	$('#sticky-div').css({ position: 'absolute', bottom: '0px', top: 'unset'})
     } else {
